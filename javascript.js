aa8d19f59d5056460086fdcb4a5b9fa352cd1319
Abby-Wheelis <54848919+Abby-Wheelis@users.noreply.github.com>
2019-11-18 16:07:02 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -63,7 +63,7 @@ var drawScatterplot = function(penguins, xScale, yScale, index)
     //.enter()
     //.append("circle")
     .transition()
-    .on("mouseover", alert("mouse"))
+    //.on("mouseover", alert("mouse")) really does not work here, does it when page loads and when button click, no dots
     .attr("fill", "blue")
     .attr("cx", function(quiz)
     {
